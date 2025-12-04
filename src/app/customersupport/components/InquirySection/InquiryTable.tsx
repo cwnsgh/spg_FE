7fe55e5f5647630b59ecb7f5a1ec6aa9c672b4c7
cwnsgh <cojuna@naver.com>
2025-12-04 09:16:33 +0900

    💄 공통레이아웃

--- src/app/customersupport/components/InquirySection/InquiryTable.tsx
+++ src/app/customersupport/components/InquirySection/InquiryTable.tsx
@@ -132,3 +132,4 @@ export default function InquiryTable({ inquiryType }: InquiryTableProps) {
     </div>
   );
 }
+
--- src/app/products/components/ProductGrid.tsx
+++ src/app/products/components/ProductGrid.tsx
@@ -86,3 +86,4 @@ export default function ProductGrid({ products }: ProductGridProps) {
   );
 }
 
+
